eea70bf991aa8313dace09175c705a703d6bec6b
CodeZhifty <juulstausholmoscar@gmail.com>
2023-05-02 10:49:09 +0200

    Added 1 aktivitet + filtrering af aktiviteter

--- scripts/aktiviteter.js
+++ scripts/aktiviteter.js
@@ -4,3 +4,15 @@ function openAktivitetModal(description) {
         aktiviteterModalBody.innerText = description;
     }, 500);
 }
+
+document.querySelectorAll("input[type='checkbox']").forEach(checkbox => {
+    checkbox.addEventListener("change", event => {
+        document.querySelectorAll(".card").forEach(card => {
+            if(!card.querySelector(".card-text").innerHTML.toLowerCase().includes(event.target.value.toLowerCase()) && event.target.checked){
+                card.style.display = "none";
+            } else {
+                card.style.display = "flex";
+            }
+        });
+    });
+});
